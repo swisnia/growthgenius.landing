a305d9d4d39971dc4828589d3e38c2cfe001dec6
swisnia <swisniowski8@gmail.com>
2023-10-25 19:12:48 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from 'src/pages/landing-page/landing-page.component';
+import { BenefitCardComponent } from 'src/components/benefit-card/benefit-card.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    BenefitCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatExpansionModule,
+    CommonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/benefit-card/benefit-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BenefitCardComponent } from './benefit-card.component';
+
+describe('BenefitCardComponent', () => {
+  let component: BenefitCardComponent;
+  let fixture: ComponentFixture<BenefitCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BenefitCardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BenefitCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
